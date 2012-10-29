9cb8c836f4fd54793df15fe6f9ade9e57083d230
aceisnotmycard <ace.is.not.my.card@gmail.com>
2012-10-29 10:49:16 +0700

    nothing special

--- src/com/example/client/SignupActivity.java
+++ src/com/example/client/SignupActivity.java
@@ -2,10 +2,12 @@ package com.example.client;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class SignupActivity extends Activity {
+public class SignupActivity extends Activity implements OnClickListener {
 	
 	EditText email, password, password_confirm;
 	Button signup;
@@ -19,5 +21,10 @@ public class SignupActivity extends Activity {
 		password = (EditText) findViewById(R.id.signup_password);
 		password_confirm = (EditText) findViewById(R.id.signup_password_confirm);
 		signup = (Button) findViewById(R.id.signup_button);
+	}
+
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
 	}
 }
